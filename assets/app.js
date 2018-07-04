e5a25c7bc6068ec555caeaf81b53fdc6fb5f8b8e
Carmen Owen <doctorsout@yahoo.com>
2018-07-04 08:33:56 -0400

    js pseudocode and linkedin link working

--- assets/app.js
+++ assets/app.js
@@ -4,3 +4,9 @@ $(function(){
       $('.sidebar').toggleClass('toggled');
     });
   });
+
+  //document.onpageload: display meCard but hide projectCard
+  //onclick of project titles/images (p1, p2, etc.) display projectCard for each
+  //onclick of About Me title/image, display meCard again
+  //populate projectCard by grabbing element ids and adding attributes for text, images, and links.
+  //create an object that holds KVPs for each project?
